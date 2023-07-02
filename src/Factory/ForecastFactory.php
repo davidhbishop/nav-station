9b36a22acf9c46f7fc72940bc70a47a209230319
David Bishop <david.bishop@agilisys.co.uk>
2023-07-02 18:08:37 +0100

    First attempt at abstract factory

---
+++ src/Factory/ForecastFactory.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Factory;
+
+/*
+ * The abstract Forecast Factory interface declares a set of methods that return different forecasts. These forecasts are called
+ * a family and are relate by a high-level theme or concept. Forecasts of one family are usually able to collaborate
+ * among themselves. A family of forecasts may have several variants, but the forecasts of one variant are imcompatible
+ * with forecasts of another.
+ */
+interface ForecastFactory
+{
+    public function createForecast(): Forecast;
+}
+
+/*
+ * Each concrete Forecast Factory corresponds to a specific variant (or family) of forecasts.
+ *
+ * This concrete Factory creates inshore forecasts
+ */
+class InshoreForecastFactory implements ForecastFactory
+{
+    public function loadForecast(): Forecast
+    {
+        return new InshoreForecast();
+    }
+}
+
+/**
+ * And this Concrete Factory creates Pressure Forecasts
+ */
+class PressureForecastFactory implements ForecastFactory
+{
+    public function createForecast(): Forecast
+    {
+        return new PressureForecast();
+    }
+}
+
+/**
+ * And this Concrete Factory creates Wind Forecasts
+ */
+class WindForecastFactory implements ForecastFactory
+{
+    public function createForecast(): Forecast
+    {
+        return new WindForecast();
+    }
+}
+
+/**
+ * Each distinct forecast type should have a seperate interface. All variants of the forecast must follow the same
+ * interface.
+ *
+ * For instance, this Abstract Forecast interface describes the behaviour of loading forecasts from file.
+ */
+interface Forecast
+{
+    public function loadForecast(): array;
+}
+
